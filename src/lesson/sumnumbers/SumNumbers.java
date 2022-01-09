97d54bf85720882dce3d0ab9d79e351738538135
ShonJava <f5df01d29806@gmail.com>
2022-01-09 20:22:26 +0500

    Сумма чисел

---
+++ src/lesson/sumnumbers/SumNumbers.java
@@ -0,0 +1,17 @@
+package lesson.sumnumbers;
+
+import java.util.Scanner;
+
+public class SumNumbers {
+    public static void main(String[] args) {
+        Scanner console = new Scanner(System.in);
+        int sum = 0;
+        System.out.println("Введите число");
+        int number = console.nextInt();
+        for (int i = 1; i <= number; i++) {
+            System.out.println(i);
+            sum += i;
+        }
+        System.out.println("Сумма чисел от 1 до " + number + "равна: " + sum);
+    }
+}
